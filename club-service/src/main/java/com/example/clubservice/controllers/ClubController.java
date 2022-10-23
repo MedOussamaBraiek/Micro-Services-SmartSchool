fc2c0b1ad48f9620a5c28237a95bf026ae966e78
alaabenfradj <73351744+alaabenfradj@users.noreply.github.com>
2022-10-23 14:49:29 +0100

    origin

--- club-service/src/main/java/com/example/clubservice/controllers/ClubController.java
+++ club-service/src/main/java/com/example/clubservice/controllers/ClubController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ClubController {
     @Autowired
     private IClubService clubService;
