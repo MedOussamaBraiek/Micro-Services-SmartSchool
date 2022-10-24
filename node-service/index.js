9490ad35471cb5e709ed7b8061d52a5f2e5d2429
tayeb <tayeb.gasmi@esprit.tn>
2022-10-24 18:04:32 +0100

    class service

--- node-service/index.js
+++ node-service/index.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const {registerWithEureka}=require("./src/eureka-client")
 var express = require("express");
+require("./src/dataBase")
 var cors = require("cors");
 var app = express();
 const route=require("./src/routes");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use("/", route);
+app.use("/classes", route);
 app.listen(process.env.PORT || 5000 , () => {
   console.log(`Server running on port ${process.env.PORT || 5000}`); 
 });
