e32c256625660620eb2a7d277a709365f37d2c94
tayeb <tayeb.gasmi@esprit.tn>
2022-10-24 16:47:39 +0100

    update club

--- course-service/src/main/java/com/example/courseservice/repositories/CourseRepository.java
+++ course-service/src/main/java/com/example/courseservice/repositories/CourseRepository.java
@@ -4,7 +4,12 @@ import com.example.courseservice.entities.Course;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CourseRepository extends MongoRepository<Course, String> {
+    public List<Course> findByName(String name);
+    public List<Course> findByCategory(String category);
 }
 
